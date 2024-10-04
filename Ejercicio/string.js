d0ecc02f5f6d32b49b25359373203fd08489dbcf
David Roa <141201627+Draixser@users.noreply.github.com>
2024-10-03 19:03:14 -0500

    Initial commit

---
+++ Ejercicio/string.js
@@ -0,0 +1,12 @@
+let string1 = "hola mundo";
+let string2 = "Este es mi ejercicio";
+let string3 = `${string1},${string2}, en react`;
+
+console.log(string3);
+
+console.log(string3.length);
+console.log(string3.toLocaleUpperCase());
+console.log(string3.toLocaleLowerCase());
+
+let frase = "javascript es lo mejor!";
+console.log(frase.substring(0, 10));
